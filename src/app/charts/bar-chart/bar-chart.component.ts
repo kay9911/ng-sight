c3acffe8339595f2975af0b8467ee2549dffd3bb
Kane <kay.padua@gmail.com>
2020-03-31 20:55:43 -0400

    Charts Intiial package and example

---
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+const SAMPLE_BARCHART_DATA: any[] = [
+  { data: [65,59,80,81,56,54,30], label: 'Fall Sales'},
+  { data: [65,59,11,12,13,25,30], label: 'Winter Sales'},
+];
+
+const SAMPLE_BARCHART_LABELS: string[] = [
+  'W1','W2','W3','W4','W5','W6','W7',
+];
+
+@Component({
+  selector: 'app-bar-chart',
+  templateUrl: './bar-chart.component.html',
+  styleUrls: ['./bar-chart.component.css']
+})
+export class BarChartComponent implements OnInit {
+
+  constructor() { }
+
+  public barChartData: any[] = SAMPLE_BARCHART_DATA;
+  public barChartLabels: string[] = SAMPLE_BARCHART_LABELS;
+  public barChartType = "bar";
+  public barChartLegend = true;
+  public barChartOptions: any = {
+    scaleShowVertiucalLines: false,
+    responsive: true
+  }
+
+  ngOnInit(): void {
+  }
+
+}
